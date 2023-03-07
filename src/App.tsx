f2ea2217d729dd63dcdfbc3b3f87b5b76eef4503
Andrés G. Carmona <ing.andres.carmona@gmail.com>
2023-03-06 20:03:08 -0600

    First version of command bar done.

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,107 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { FC, useState } from 'react'
 
-function App() {
-  const [count, setCount] = useState(0)
+interface Command {
+  title: string
+  desc: string
+  type: string
+  action: string
+  emoji: boolean
+  emojiChar: string
+  keyCheck: boolean
+}
 
+const defaultCommands: Command[] = [
+  {
+    title: 'Search',
+    desc: 'Search for a query',
+    type: 'action',
+    action: 'search',
+    emoji: true,
+    emojiChar: '🔍',
+    keyCheck: false,
+  },
+  {
+    title: 'Search',
+    desc: 'Go to website',
+    type: 'action',
+    action: 'goto',
+    emoji: true,
+    emojiChar: '🔍',
+    keyCheck: false,
+  },
+]
+
+const Command: FC<{ command: Command }> = ({ command }) => {
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="warp-item">
+      <span className="warp-emoji-action">{command.emojiChar}</span>
+  
+      <div className="warp-item-details">
+        <div className="warp-item-name">{command.title}</div>
+        <div className="warp-item-desc">{command.desc}</div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </div>
   )
 }
 
-export default App
+export const App = () => {
+  const [isOpen, setIsOpen] = useState(false)
+  const [commands, setCommands] = useState<Command[]>(defaultCommands)
+  
+  type Action = 'open-warp' | 'close-warp'
+  
+  interface ActionMessage {
+    action: Action
+  }
+  
+  const openWarp = () => {
+    setIsOpen(true)
+  }
+  
+  const closeWarp = () => {
+    setIsOpen(false)
+  }
+  
+  // Listen messages from background
+  chrome.runtime?.onMessage.addListener((message: ActionMessage) => {
+    if (message.action == 'open-warp') {
+      if (isOpen) {
+        closeWarp()
+      }
+      else {
+        openWarp()
+      }
+    }
+    
+    if (message.action == 'close-warp') {
+      closeWarp()
+    }
+  })
+  
+  return (
+    <>
+      <div id="warp-extension" className={`${!isOpen ? 'warp-closin' : ''} warp-extension`}>
+        <div id="warp-wrap">
+          <div id="warp">
+            <div id="warp-search">
+              <input placeholder="Type a command or search"/>
+            </div>
+            <div id="warp-list">
+              {commands.map((command, index) => (
+                <Command key={index} command={command} />
+              ))}
+            </div>
+            <div id="warp-footer">
+              <div id="warp-results">{commands.length} results</div>
+              <div id="warp-arrows">Use arrow keys <span className="warp-shortcut">↑</span><span
+                className="warp-shortcut">↓</span> to navigate
+              </div>
+            </div>
+          </div>
+        </div>
+        <div id="warp-overlay"></div>
+      </div>
+      <div id="warp-extension-toast"><img alt="" src=""/><span>The action has been successful</span></div>
+    </>
+  )
+}
--- src/background.ts
+++ src/background.ts
@@ -1,5 +1,54 @@
-chrome.runtime.onInstalled.addListener(function() {
-  chrome.storage.sync.set({color: '#3aa757'}, function() {
-    console.log("The color is green.")
+function injectScripts(tab: any) {
+  const manifest = chrome.runtime.getManifest() || {}
+  
+  const scripts = manifest.content_scripts && manifest.content_scripts[0]
+
+  if (scripts && Array.isArray(scripts.js)) {
+    for (let script of scripts.js) {
+      chrome.scripting.executeScript({
+        target: { tabId: tab.id },
+        files: [script]
+      })
+    }
+  
+    if (scripts && Array.isArray(scripts.css)) {
+      chrome.scripting.insertCSS({
+        target: { tabId: tab.id },
+        files: [scripts.css[0]],
+      })
+    }
+  }
+}
+
+async function getCurrentTab() {
+  const queryOptions = { active: true, currentWindow: true };
+  const [tab] = await chrome.tabs.query(queryOptions);
+  return tab;
+}
+
+chrome.runtime.onInstalled.addListener(function () {
+  chrome.storage.sync.set({ installed: true }, function () {
+    console.log('The extension was successfully installed.')
   })
+  
+  chrome.windows.getAll(
+    {
+      populate: true,
+    },
+    (windows) => {
+      console.log(windows)
+      
+      for (let window of windows) {
+        for (let tab of window.tabs ?? []) {
+          if (tab && !tab.url?.includes("chrome://") && !tab.url?.includes("chrome-extension://") && !tab.url?.includes("chrome.google.com")) {
+            injectScripts(tab);
+          }
+        }
+      }
+    }
+  );
 })
+
+chrome.action.onClicked.addListener((tab) => {
+  chrome.tabs.sendMessage(tab.id as number, {action: "open-warp"});
+});
